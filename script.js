23365c3e6c75ccb8a54cd3aca78f17c400f9f5c3
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-08-04 00:07:49 -0300

    Adicionando a troca de imagens com a troca do mood

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ function toggleMode() {
   } else {
     html.classList.add("light")
   } */
+
+  /*   Troca de Imagem */
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a img light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // Se tiver sem light mode, manter a img normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
